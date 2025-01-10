dc95692babce245b8dd7008d84a16282814008fe
PoisonNF <903131263@qq.com>
2025-01-10 17:03:40 +0800

    巡线，限制力和力矩的大小，效果还可以

--- Parameters.m
+++ Parameters.m
@@ -7,7 +7,7 @@ Iyy = 5.98; %机身y轴转动惯量
 Izz = 9.1;  %机身z轴转动惯量
 %}
 
-m = 32.135;    %质量为10kg
+m = 35.135;    %质量为10kg
 Ixx = 1.4676;  %机身x轴转动惯量
 Iyy = 1.4676;  %机身y轴转动惯量
 Izz = 2.4720;  %机身z轴转动惯量
@@ -52,93 +52,3 @@ SW的坐标轴和普通的不一样，Y朝上
 	Iyx = 5625.578	Iyy = 2472061.553	Iyz = 5574.990
 	Izx = -59590.923	Izy = 5574.990	Izz = 1467666.836
 %}
-
-%%
-%方形
-desired_pos_series = [
-0,0,0,0;           
-0,0,5,0;
-           5,0,5,0;
-           5,5,5,0;
-           0,5,5,0;
-           0,0,5,0];%期望路径x,y,z(z是正的表示高度,到后面应该乘个负号)
-desired = desired_pos_series;
-hold on
-grid on
-figure(1)
-plot3(desired_pos_series(:,1),desired_pos_series(:,2),desired_pos_series(:,3),'-r');
-xlabel('x');
-ylabel('y');
-zlabel('z');
-title("方型轨迹仿真结果");
-xlim([-3,8]);
-ylim([-3,8]);
-zlim([0,8]);
-
-
-%%
-%圆形
-%确定航轨点数目
-num_points =100;
-heigh = 3;%设置高度
-%预先生成储存x、y位置的向量
-x = zeros(num_points,1);
-y = zeros(num_points,1);
-z = ones(num_points,1);
-%存储期望的轨迹点
-desired = zeros(num_points+2,4);
-desired_pos_series = zeros(num_points+2,3);
-desired_yaw = zeros(num_points+1,1);
-desired_pos_series(1,:)=[0,0,0];
-desired_pos_series(2,:)=[0,0,heigh];
-%生成轨迹点
-for i = 1:num_points
-    x(i) = 10*cos(2*pi*i/num_points);
-    y(i) = 10*sin(2*pi*i/num_points);
-end
-%给轨迹点赋值
-desired_pos_series(3:end,1) = x(:,1);
-desired_pos_series(3:end,2) = y(:,1);
-desired_pos_series(3:end,3) = z(:,1)*heigh;
-desired(:,1:3) = desired_pos_series(:,:);
-desired(2:end,4) = desired_yaw;
-hold on
-grid on
-figure(1)
-plot3(desired_pos_series(:,1),desired_pos_series(:,2),desired_pos_series(:,3),'-r');
-xlabel('x');
-ylabel('y');
-zlabel('z');
-title("圆形轨迹仿真结果");
-xlim([-15,15]);
-ylim([-15,15]);
-zlim([0,5]);
-
-%%
-%梳状面
-desired_pos_series = [
-0,0,0,0;           
-0,0,5,0;
-           5,0,5,0;
-           5,1,5,0;
-           0,1,5,0;
-           0,2,5,0;
-           5,2,5,0;
-           5,3,5,0;
-           0,3,5,0;
-           0,4,5,0;
-           5,4,5,0;
-           5,5,5,0;
-           ];%期望路径x,y,z(z是正的表示高度,到后面应该乘个负号)
-desired = desired_pos_series;
-hold on
-grid on
-figure(1)
-plot3(desired_pos_series(:,1),desired_pos_series(:,2),desired_pos_series(:,3),'-r');
-xlabel('x');
-ylabel('y');
-zlabel('z');
-title("方型轨迹仿真结果");
-xlim([-3,8]);
-ylim([-3,8]);
-zlim([0,8]);
